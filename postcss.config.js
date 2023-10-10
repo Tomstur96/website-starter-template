caf6b0cbb430b11643d3cf7a132b0d498e9cac74
Thomas <tomstur96flightsim@gmail.com>
2023-10-10 13:28:30 +1100

    added features and browser-list

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,23 @@ const postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
   plugins: [
     postcssPresetEnv({
-      stage: 0,
+      stage: 2,
+      features: {
+        "logical-properties-and-values": false,
+      },
     }),
   ],
 };
+
+// --- Inverse Option but with specific features --- //
+
+// module.exports = {
+//   plugins: [
+//     postcssPresetEnv({
+//       stage: 3,
+//       features: {
+//         "trigonometric-functions": true,
+//       },
+//     }),
+//   ],
+// };
